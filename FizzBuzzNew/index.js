f2b0543457f4b6dd4d5c8ff1601082dd34726720
Filipp Gorbunov <Filippgorb@gmail.com>
2018-06-14 12:07:23 -0700

    Completed FizzBuzz Problem

--- FizzBuzzNew/index.js
+++ FizzBuzzNew/index.js
@@ -3,15 +3,25 @@
 $(function() {
   $('#number-chooser').on('submit', event => {
     event.preventDefault();
-    let answer = 
-
-
-
-
-
-
-
-  })
+    $('.js-results').empty();
+    let inputValue = $('#number-choice').val();
+    console.log(inputValue);
+    const result = [];
+    for (let i = 1; i <= inputValue; i++) {
+      if (i % 15 === 0) {
+        result.push($('<div class="fizz-buzz-item fizzbuzz"><span>fizzbuzz</span></div>'));
+      } else if (i % 5 === 0) {
+        result.push($('<div class="fizz-buzz-item buzz"><span>buzz</span></div>'));
+      } else if (i % 3 === 0) {
+        result.push($('<div class="fizz-buzz-item fizz"><span>fizz</span></div>'));
+      } else {
+        result.push($(`<div class="fizz-buzz-item"><span>${i}</span></div>`));
+      }
+      //console.log(result);
+    }
+    $('.js-results').append(result);  
+  }  
+  );
 });
   
 
